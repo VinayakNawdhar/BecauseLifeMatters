cf25b728e122f8f6dcd8ba1577a77225e80c05c9
Vinayak Nawdhar <vinayaknawdhar003@gmail.com>
2023-06-24 01:11:53 +0530

    update

--- Public/index.js
+++ Public/index.js
@@ -30,7 +30,7 @@ btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 
 btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+// overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
