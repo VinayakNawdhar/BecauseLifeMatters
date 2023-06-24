452de987554bc68ee6357b3ff206a08458513e9a
Vinayak Nawdhar <vinayaknawdhar003@gmail.com>
2023-06-24 11:23:53 +0530

    update

--- form.js
+++
@@ -1,9 +0,0 @@
-const name = document.getElementById("name").value;
-const email = document.getElementById("email").value;
-const address = document.getElementById("address").value;
-const btn = document.querySelector(".sendBtn");
-
-btn.addEventListener('click',function(e){
-    e.preventDefault();
-    console.log(name,email,address);
-})
